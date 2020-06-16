b9ae1d36aac87143684af06d6beceda74edc7d39
Claudiu <cduneata@gmail.com>
2020-06-16 12:32:11 +0300

    Add thread model to display threads for specific process

---
+++ ProcessNote/ProcessNote/Model/ThreadModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessNote.Model
+{
+    class ThreadModel
+    {
+        public int Id { get; set; }
+        public int Priority { get; set; }
+        public int Started { get; set; }
+
+        private List<ProcessThread> processThreadsList;
+
+        public ThreadModel()
+        {
+            processThreadsList = new List<ProcessThread>();
+        }
+
+        private void GetAllProcessThreads(Process process)
+        {
+            ProcessThreadCollection threads = process.Threads;
+
+            foreach(ProcessThread thread in threads)
+            {
+                processThreadsList.Add(thread);
+            }
+        }
+
+        public List<ProcessThread> GetThreadsList()
+        {
+            return processThreadsList;
+        }
+
+    }
+}
