dc174ea6d2a6676ade0fbd832a98ba9d7a5df597
Stefan Gherman <scgheman@gmail.com>
2020-06-18 09:24:14 +0300

    Add StatsWindow code behind file with singleton

---
+++ ProcessNote/ProcessNote/StatsWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ProcessNote
+{
+    /// <summary>
+    /// Interaction logic for StatsWindow.xaml
+    /// </summary>
+    public partial class StatsWindow : Window
+    {
+        private static StatsWindow instance;
+        private DispatcherTimer _mainWindowTimer;
+        private MainWindow mainWindow;
+        public  static StatsWindow GetInstance(MainWindow mainWindow, DispatcherTimer timer)
+        {
+            if (instance == null)
+            {
+                instance = new StatsWindow(mainWindow, timer);                    
+            }
+            
+            return instance;
+        }
+        private StatsWindow(MainWindow mainWindow, DispatcherTimer timer)
+        {
+            this.mainWindow = mainWindow;
+            _mainWindowTimer = timer;
+            mainWindow.performanceViewer.IsEnabled = false;
+            InitializeComponent();
+            _mainWindowTimer.Stop();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            _mainWindowTimer.Start();
+            mainWindow.performanceViewer.IsEnabled = true;
+        }
+    }
+}
